ccaf8c397dc3da76456cc1a014c90aaf1e4467a5
Eremel <richardbailey@outlook.com>
2025-02-09 00:37:45 +0000

    oopsies :)

--- src/utils.lua
+++ src/utils.lua
@@ -831,14 +831,14 @@ function time(func, ...)
     return 1000*(end_time-start_time)
 end
 
-function Card:add_sticker(self, sticker, bypass_check)
+function Card:add_sticker(sticker, bypass_check)
     local sticker = SMODS.Stickers[sticker]
     if bypass_check or (sticker and sticker.should_apply and type(sticker.should_apply) == 'function' and sticker:should_apply(self, self.config.center, self.area, true)) then
         sticker:apply(self, true)
     end
 end
 
-function Card:remove_sticker(self, sticker)
+function Card:remove_sticker(sticker)
     if self.ability[sticker] then
         SMODS.Stickers[sticker]:apply(self, false)
     end
