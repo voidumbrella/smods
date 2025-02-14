7744bacfa3bade7fe456eefa71692977cab9b801
Lolo <lowgau@gmail.com>
2025-02-14 22:03:33 +0100

    Add french localization (#459)
    * french localization
    
    * Remove out of place capitalization

---
+++ localization/fr.lua
@@ -0,0 +1,132 @@
+return {
+    descriptions = {
+        Other = {
+            load_success = {
+                text = {
+                    'Mod chargé',
+                    '{C:green}avec succès!'
+                }
+            },
+            load_failure_d = {
+                text = {
+                    'Dépendances {C:attention}manquantes!',
+                    '#1#',
+                }
+            },
+            load_failure_c = {
+                text = {
+                    'Conflits {C:attention}non résolus!',
+                    '#1#'
+                }
+            },
+            load_failure_d_c = {
+                text = {
+                    'Dépendances {C:attention}manquantes!',
+                    '#1#',
+                    'Conflits {C:attention}non résolus!',
+                    '#2#'
+                }
+            },
+            load_failure_o = {
+                text = {
+                    '{C:attention}Obsolète!{} Steamodded',
+                    'versions {C:money}0.9.8{} et dessous',
+                    'ne sont plus supportés.'
+                }
+            },
+            load_failure_i = {
+                text = {
+                    '{C:attention}Incompatible!{} Nécessite version',
+                    '#1# de Steamodded,',
+                    'mais #2# est installé.'
+                }
+            },
+            load_failure_p = {
+                text = {
+                    '{C:attention}Conflit de Préfixe!{}',
+                    'Le préfixe de ce mod est',
+                    'le même que celui d\'un autre mod.',
+                    '({C:attention}#1#{})'
+                }
+            },
+            load_failure_m = {
+                text = {
+                    '{C:attention}Fichier principal manquant!{}',
+                    'Le fichier principal de ce mod',
+                    'n\'a pas pu être trouvé.',
+                    '({C:attention}#1#{})'
+                }
+            },
+            load_disabled = {
+                text = {
+                    'Ce mod a été',
+                    '{C:attention}désactivé!{}'
+                }
+            }
+        },
+        Edition = {
+            e_negative_playing_card = {
+                name = "Négatif",
+                text = {
+                    "{C:dark_edition}+#1#{} à la taille de la main"
+                },
+            },
+        }
+    },
+    misc = {
+        achievement_names = {
+            hidden_achievement = "???",
+        },
+        achievement_descriptions = {
+            hidden_achievement = "Jouez plus pour découvrir!",
+        },
+        dictionary = {
+            b_mods = 'Mods',
+            b_mods_cap = 'MODS',
+            b_modded_version = 'Version Moddé!',
+            b_steamodded = 'Steamodded',
+            b_credits = 'Crédits',
+            b_open_mods_dir = 'Ouvrir le répertoire des mods',
+            b_no_mods = 'Aucun mod détecté...',
+            b_mod_list = 'Liste des mods activés',
+            b_mod_loader = 'Mod Loader',
+            b_developed_by = 'développé par ',
+            b_rewrite_by = 'Réécrit par ',
+            b_github_project = 'Projet Github',
+            b_github_bugs_1 = 'Vous pouvez signaler des bugs et',
+            b_github_bugs_2 = 'y faire des contributions.',
+            b_disable_mod_badges = 'Désactiver les badges de mod',
+            b_author = 'Auteur',
+            b_authors = 'Auteurs',
+            b_unknown = 'Inconnu',
+            b_lovely_mod = '(Mod Lovely) ',
+            b_by = ' Par : ',
+			b_config = "Config",
+			b_additions = 'Ajouts',
+      		b_stickers = 'Autocollants',
+			b_achievements = "Succès",
+      		b_applies_stakes_1 = 'Applique ',
+			b_applies_stakes_2 = '',
+			b_graphics_mipmap_level = "Niveau Mipmap",
+			b_browse = 'Parcourir',
+			b_search_prompt = 'Chercher des mods',
+			b_search_button = 'Recherche',
+            b_seeded_unlocks = 'Progression si Seed custom',
+            b_seeded_unlocks_info = 'Permet la progression et les découvertes pour les parties Seedées.',
+            ml_achievement_settings = {
+                'Désactivé',
+                'Activé',
+                'Sans restrictions'
+            },
+            b_deckskins_lc = 'Couleurs Faible Contraste',
+            b_deckskins_hc = 'Couleurs Haut Contraste',
+            b_deckskins_def = 'Couleurs par défaut',
+		},
+		v_dictionary = {
+			c_types = '#1# Types',
+			cashout_hidden = '...et #1# de plus',
+            a_xchips = "X#1# Jetons",
+            a_xchips_minus = "-X#1# Jetons",
+		},
+	}
+}
