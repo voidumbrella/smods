d01bfeea205f818787177a53a76526a05a25c997
vitellaryjr <43586575+vitellaryjr@users.noreply.github.com>
2025-03-20 18:29:53 -0700

    fix deprecated gradient warn crash (#571)

--- src/game_object.lua
+++ src/game_object.lua
@@ -765,7 +765,7 @@ Set `prefix_config.key = false` on your object instead.]]):format(obj.key), obj.
             G.C.RARITY[self.key] = self.badge_colour
             -- Called every frame, moving deprecated prints here
             if self.gradient and type(self.gradient) == "function" then
-                sendWarnMessage(('Found `gradient` function on SMODS.Rarity object "%s". This field is deprecated; please use `SMODS.Gradient` API instead.'):format(obj.key), 'Rarity')
+                sendWarnMessage(('Found `gradient` function on SMODS.Rarity object "%s". This field is deprecated; please use `SMODS.Gradient` API instead.'):format(self.key), 'Rarity')
             end
         end,
         process_loc_text = function(self)
