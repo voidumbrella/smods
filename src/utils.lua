abee4052a16b49030e0d3352e95f2717f5d30454
Kekulism <33201764+Kekulism@users.noreply.github.com>
2025-03-21 16:21:28 -0400

    Smeared Joker and Seeing Double bug fix with Modded Suits (#563)
    * Update enhancement.toml
    
    * Revert "Update enhancement.toml"
    
    This reverts commit 3a22653a6cbf1dd17e2b130bacac2d9a18759f5b.
    
    * Smeared Joker and Seeing Double bug fix with Modded Suits
    
    - Fixes bug where Smeared Joker would count any modded suit as a Club or Spade
    - Fixes bug that prevented a hand with 1 Club and all other modded suits from getting a X2 with Seeing Double

--- src/utils.lua
+++ src/utils.lua
@@ -2072,3 +2072,52 @@ function SMODS.multiplicative_stacking(base, perma)
     local ret = base * perma
     return (ret == 1 and 0) or (ret > 0 and ret) or 0
 end
+
+function SMODS.smeared_check(card, suit)
+    if ((card.base.suit == 'Hearts' or card.base.suit == 'Diamonds') and (suit == 'Hearts' or suit == 'Diamonds')) then
+        return true
+    elseif (card.base.suit == 'Spades' or card.base.suit == 'Clubs') and (suit == 'Spades' or suit == 'Clubs') then
+        return true
+    end
+    return false
+end
+
+local function has_any_other_suit(count, suit)
+    for k, v in pairs(count) do
+        if k ~= suit then
+            if v > 0 then
+                return true
+            end
+        end
+    end
+    return false
+end
+
+local function saw_double(count, suit)
+    if count[suit] > 0 and has_any_other_suit(count, suit) then return true else return false end
+end
+
+function SMODS.seeing_double_check(hand, suit)
+    local suit_tally = {}
+    for i = #SMODS.Suit.obj_buffer, 1, -1 do
+        suit_tally[SMODS.Suit.obj_buffer[i]] = 0
+    end
+    for i = 1, #hand do
+        if not SMODS.has_any_suit(hand[i]) then
+            for k, v in pairs(suit_tally) do
+                if hand[i]:is_suit(k) then suit_tally[k] = suit_tally[k] + 1 end
+            end
+        elseif SMODS.has_any_suit(hand[i]) then
+            if hand[i]:is_suit('Clubs') and suit_tally["Clubs"] == 0 then suit_tally["Clubs"] = suit_tally["Clubs"] + 1
+            elseif hand[i]:is_suit('Diamonds') and suit_tally["Diamonds"] == 0  then suit_tally["Diamonds"] = suit_tally["Diamonds"] + 1
+            elseif hand[i]:is_suit('Spades') and suit_tally["Spades"] == 0  then suit_tally["Spades"] = suit_tally["Spades"] + 1
+            elseif hand[i]:is_suit('Hearts') and suit_tally["Hearts"] == 0  then suit_tally["Hearts"] = suit_tally["Hearts"] + 1 end
+            for k, v in pairs(suit_tally) do
+                if k ~= "Clubs" and k ~= "Diamonds" and k ~= "Hearts" and k ~= "Spades" then
+                    if hand[i]:is_suit(k) and suit_tally[k] == 0  then suit_tally[k] = suit_tally[k] + 1 end
+                end
+            end
+        end
+    end
+    if saw_double(suit_tally, suit) then return true else return false end
+end
